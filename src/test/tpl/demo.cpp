1c04200ede53835acd040df5b0b48f33d7d8a814
Anteoy <anteoy@gmail.com>
2018-04-16 21:49:11 +0800

    add tpl

---
+++ src/test/tpl/demo.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+template <typename T>
+void display(T a)
+{
+    cout << a << endl;
+}
+
+template <typename T,class C>
+void display(T t,C c)
+{
+    cout << t << endl;
+    cout << c << endl;
+}
+
+template <typename T,int a>
+void display(T t)
+{
+    for (int i = 0;i<a;i++) 
+    {
+        cout << a << endl;
+    }
+}
+
+int main(void)
+{
+    display<int>(10);
+    display<double>(10.58);
+    display<int,double>(11,11.22);
+    display<int,3>(6);
+    return 0;
+}
