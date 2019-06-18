bd6cdd0aac0d23cb74d1f89e7b082363e1cc4de7
anteoy <anteoy@gmail.com>
2019-06-18 17:13:39 +0800

    add: datastructure

--- src/monitor/new_data_structure/recur/ArrayMin.cc
+++ src/monitor/new_data_structure/recur/ArrayMin.cc
@@ -32,7 +32,9 @@ int main(){
     float A[7] = {1,6,4,8,9,23,-1};
     double m = f(A,6);
     cout << "最小值为: " << m << endl;
-    float *B = new float[7];
+//    float *B = new float[7];
+    // or
+    float *B = malloc(7*sizeof(int))
     for (int i=0;i<7;i++) {
         *(B+i) = i+10;
     }
@@ -43,5 +45,7 @@ int main(){
 //    double mb = f(B,6);//两种方式都可以
     double mb = f2(B,6);
     cout << "最小值为: " << mb << endl;
-    delete [] B;
+//    delete [] B;
+// or
+    free(B);
 }
